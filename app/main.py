dcba25f8949053714a28b3eac6ef3746378cd0f4
Seraphim <Nihaz.444.s.d@mail.ru>
2026-03-05 21:36:46 +0300

    Добавил описание, обновил requirements, не работал с версией pydantic_core 2.42.0

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from app.api.v1.routers import router as task_router
 from app.core.db import engine
 from app.models.task import Base
 
-app = FastAPI(title="Task Tracker API")
+app = FastAPI(title="Task Tracker API",
+              description="Небольшой трекер задач в качестве учебного проекта.",
+              version="1.0.0")
 
 app.include_router(task_router)
 
